1a8f190345d4bda8855b9d7871dd6af0ef9a244d
jonnyz <dev.jonny255d@gmail.com>
2022-08-20 17:52:38 -0300

    update

--- src/Data/Apis/Amiibo/AmiiboApi.ts
+++ src/Data/Apis/Amiibo/AmiiboApi.ts
@@ -3,6 +3,7 @@ import api from "../Base/api";
 const getList = () => api.get("/amiibo");
 const getDetails = (id: string) => api.get("/amiibo?tail=" + id);
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     getList,
     getDetails
